d929151e9227b88e531859afc951a6b1ff0e4890
Raahim2 <127599591+Raahim2@users.noreply.github.com>
2025-08-02 19:59:06 +0530

    noDB

--- PlotPy/settings.py
+++ PlotPy/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'PlotPy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
